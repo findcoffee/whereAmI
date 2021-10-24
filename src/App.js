24726ce8632bc04f4e0fe222f6b434b01b2359af
SangJun Park <sjpark042@gaonpf.com>
2021-10-25 08:14:06 +0900

    add new files testing version.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import { FaBeer, FaHome } from 'react-icons/fa';
+import { CgHomeAlt } from 'react-icons/cg';
+import {
+  HomeOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+  LoadingOutlined,
+} from '@ant-design/icons';
+import { Space, Card, Row, Col } from 'antd';
+import { blue } from '@ant-design/colors';
 
 function App() {
+  console.log(blue);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row gutter={[24, 24]}>
+        <Col span={6} color={blue[1]}/>
+        <Col span={6} />
+        <Col span={6} />
+        <Col span={6} />
+
+        <Col span={6} />
+        <Col span={6} />
+        <Col span={6} />
+        <Col span={6} />
+      </Row>
+      <Row gutter={[24, 24]}>
+        <Col span={6} />
+        <Col span={6} />
+        <Col span={6} />
+        <Col span={6} />
+      </Row>
+    {/* <Space direction="vertical">
+      <Card title="Card" style={{ width: 300 }}>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      <Card title="Card" style={{ width: 300 }}>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    </Space> */}
+      <div> Beer <FaBeer/> </div>
+      <div> Home <FaHome/> </div>
+      <div> Home Alt <CgHomeAlt/> </div>
+      <div className="icons-list">
+        <HomeOutlined />
+        <SettingFilled />
+        <SmileOutlined />
+        <SyncOutlined spin />
+        <SmileOutlined rotate={180} />
+        <LoadingOutlined />
+      </div>
     </div>
   );
 }
